854c02531a08a8e06415bbe71c8d9bd9cffb2ba4
martindwyer <martin@mdbytes.com>
2021-11-27 09:15:00 -0600

    CssGridCourse File Updates

--- BootstrapSass/js/script.js
+++ BootstrapSass/js/script.js
@@ -8,6 +8,7 @@ const lightbox = GLightbox({
   autoPlayVideos: "true",
 });
 
+const navToggleLink = document.querySelector("#navToggler");
 const homeLink = document.querySelector("#homeLink");
 const servicesLink = document.querySelector("#servicesLink");
 const testimonialsLink = document.querySelector("#testimonialsLink");
@@ -16,6 +17,13 @@ const portfolioLink = document.querySelector("#portfolioLink");
 const contactLink = document.querySelector("#contactLink");
 
 const navToggler = document.querySelector("#navToggler");
+
+const buttonClick = () => {
+  contactLink.click();
+  setTimeout(function () {
+    navToggleLink.click();
+  }, 500);
+};
 
 homeLink.addEventListener("click", () => {
   console.log("nav link clicked");
